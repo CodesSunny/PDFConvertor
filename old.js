c46f960156604b1207ed039d50b9359a96445118
TechSunny <sunnymotivation2@gmail.com>
2024-10-23 22:19:23 +0530

    setting width of pdf

--- old.js
+++ old.js
@@ -6,20 +6,15 @@ const textArea= document.getElementById("text-area");
 let inputFileText="";
 
 const createPdf=()=>{
+    // textarea to pdf
     const doc = new jsPDF();  // create instance
-    // Coordinates for the text starting point
-    const x = 10;
-    const y = 20;
-    const maxWidth = 190;  // The width from x = 10 to x = 100
     const areaText=textArea.value;
-    if(areaText){    // textarea to pdf
-        // Split the text into multiple lines to fit within the specified width
-        const wrappedText = doc.splitTextToSize(areaText, maxWidth);
-        doc.text(wrappedText, x, y);
+    if(areaText){
+        doc.text(areaText, 10, 10);
         doc.save("doc1.pdf"); //download pdf file
-    }else if(inputFileText){   // input file to pdf
-        const wrappedText = doc.splitTextToSize(inputFileText, maxWidth);
-        doc.text(wrappedText, x, y);      
+    }else if(inputFileText){
+        // input file to pdf
+        doc.text(inputFileText, 10, 10);
         doc.save("doc1.pdf"); //download pdf file           
     }else{
         alert("Either upload a file or type text")
